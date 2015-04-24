f05aa0e62a6f19d64429f671d04828274b68eb5d
Sam <hibbards01@gmail.com>
2015-04-24 09:13:53 -0600

    First commit.

---
+++ sweetness/findSweetness.cpp
@@ -0,0 +1,22 @@
+/****************************************************
+* Program:
+*   findSweetness.cpp
+* Author:
+*   Samuel Hibbard
+* Summary:
+*   This will be testing out the range of Euler's
+*       plynomial.
+****************************************************/
+
+#include <iostream>
+using namespace std;
+
+/*************************
+* main
+*   Driver function.
+*************************/
+int int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
