2b9b0436516db298e91e6fa21bd872199a8664cd
Sam <hibbards01@gmail.com>
2015-04-24 09:43:15 -0600

    Added some more functions.

--- sweetness/findSweetness.cpp
+++ sweetness/findSweetness.cpp
@@ -9,7 +9,39 @@
 ****************************************************/
 
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+/**************************************
+* testForPrimality
+*   This function will be using the
+*     Sieve of Eratosthenes to find
+*     if the number you have is a Prime
+*     number.
+**************************************/
+bool testForPrimality(int num)
+{
+
+}
+
+/*******************************************************
+* testEulersPolynomial
+*   This will have a given range, and will
+*     test that range using Euler's Polynomial function
+*     and then check it's primality.
+*******************************************************/
+void testEulersPolynomial(int min, int max, int c)
+{
+    // Start from the min and work our way up to the max.
+    for (int i = min; i < max; ++i)
+    {
+        // Now use Euler's Polynomial!
+        //  num =     x^2 + x + C
+        int num = (i * i) + i + c;
+
+        // Now test for it's primality!
+    }
+}
 
 /*************************
 * main
